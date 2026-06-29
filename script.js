e5f75b0b0c1f893633695f97a5914f09893980d0
lakshuu07 <kaushiklakshay90@gmail.com>
2026-06-30 00:51:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-THIS IS IMPORTANT
+function checkPassword() {
+    let pass = document.getElementById("pass").value;
+
+    if (pass === "30102025") {
+        document.getElementById("video").style.display = "block";
+    } else {
+        alert("Wrong password ❤️");
+    }
+}
